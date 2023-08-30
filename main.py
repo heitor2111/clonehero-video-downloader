6553297a4a63a4d713acc6e15f28d22e858eb5b2
Heitor Henrique <heitor2111@gmail.com>
2023-08-30 11:29:16 -0300

    limitador de duração do video adicionado

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import os
 import platform
 import sys
 import traceback
-from typing import Optional
+
+from alive_progress import alive_bar, alive_it
 
 # Get the source folder
 if platform.system() == 'Windows':
     sourceFolder = os.path.expanduser('~') + r'\Documents\Clone Hero'
+    os.system('cls')
 elif platform.system() == 'Linux':
     sourceFolder = os.path.basename('~') + r'\.clonehero'
+    os.system('clear')
 elif platform.system() == 'Darwin':
     sourceFolder = os.path.basename('~') + r'\Clone Hero'
+    os.system('clear')
 else:
     sourceFolder = ''
 
@@ -32,7 +36,7 @@ for arg in args:
         print('  --src=<path>       Path to the Clone Hero folder.')
         print('  --replace          Replace downloaded songs.')
         print('  --help             Show this help message.')
-        sys.exit(0)
+        sys.exit(1)
 
 # If not exists Songs or PlayerData/Songs folder, exit the program with message
 if not os.path.isdir(sourceFolder + r'\Songs'):
@@ -48,16 +52,15 @@ else:
 
 # Create a object of the class Songs
 class Songs:
-    def __init__(self, name, path, video):
+    def __init__(self, name, path, video, song_length):
         self.name = name
         self.path = path
         self.video = video
+        self.song_length = song_length
 
 
 # Create a empty list to store the objects of the class Songs
 songs = []
-
-from alive_progress import alive_bar, alive_it
 
 # Count the total number of files
 folders = os.walk(sourceFolder)
@@ -67,7 +70,7 @@ total_files = sum(len(files) for _, _, files in folders)
 with alive_bar(total_files, bar="smooth", title="Verificando arquivos...") as bar:
     for root, dirs, files in os.walk(sourceFolder):
         for file in files:
-            song = Songs('', root, False)
+            song = Songs('', root, False, 0)
 
             if file == 'song.ini':
                 # Open the song.ini file supporting UTF-8 and read the content
@@ -79,6 +82,8 @@ with alive_bar(total_files, bar="smooth", title="Verificando arquivos...") as ba
                             artist = line.split('=')[1].strip()
                         elif line.startswith('name'):
                             name = line.split('=')[1].strip()
+                        elif line.startswith('song_length'):
+                            song.song_length = int(line.split('=')[1].strip()) / 1000
 
                         if 'artist' in locals() and 'name' in locals():
                             song.name = artist + ' - ' + name
@@ -96,12 +101,12 @@ with alive_bar(total_files, bar="smooth", title="Verificando arquivos...") as ba
             # Update the progress bar
             bar()
 
-# Print a empty line to space the progress bar from the list
-print('\n')
-
 # If replaceDownloaded is False, filter the list to show only songs without video
 if not replaceDownloaded:
     songs = list(filter(lambda x: x.video is False, songs))
+
+# Print a count of songs selected
+print(f'{len(songs)} songs found.')
 
 # Search for video in YouTube and try to download it
 songs = alive_it(songs, bar="smooth", title="Downloading videos...", dual_line=True)
@@ -115,22 +120,27 @@ for song in songs:
         # Search for the video in YouTube
         search = Search(f"{song.name} Official Video")
 
-        yt = search.results[0] if len(search.results) > 0 else None
+        yt = None
 
-        if yt is None:
+        if len(search.results) > 0:
+            for result in search.results:
+                if song.song_length + 15 >= result.length >= song.song_length - 5:
+                    yt = result
+                    break
+
+        if yt:
+            # Get the video in the best resolution and extract only image
+            for stream in yt.streams.filter(only_video=True, file_extension='mp4').order_by('resolution').desc():
+                # Check if the video resolution is 1080p or less
+                if int(stream.resolution.replace('p', '')) <= 1080:
+                    stream.download(song.path, "video.mp4")
+
+                    # Print a message
+                    print(f'{song.name}: Downloaded successfully.')
+
+                    break
+        else:
             print(f'{song.name}: Video not found.')
-            break
-
-        # Get the video in the best resolution and extract only image
-        for stream in yt.streams.filter(only_video=True, file_extension='mp4').order_by('resolution').desc():
-            # Check if the video resolution is 1080p or less
-            if int(stream.resolution.replace('p', '')) <= 1080:
-                stream.download(song.path, "video.mp4")
-
-                # Print a message
-                print(f'{song.name}: Downloaded successfully.')
-
-                break
     except:
         print('An error occurred while downloading the video.')
 
